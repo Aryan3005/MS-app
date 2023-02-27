1adbf95a897c3c98af1e973ee4baa936d47fac09
Kaival <87368466+Kaival1212@users.noreply.github.com>
2023-02-27 17:17:52 +0000

    Api Testing (Not final)

---
+++ MUSIC APP/Api_try.swift
@@ -0,0 +1,8 @@
+//
+//  Api_try.swift
+//  MUSIC APP
+//
+//  Created by Kaival Patel on 21/02/2023.
+//
+
+import Foundation
---
+++ MUSIC APP/Api_try_Spotify.swift
@@ -0,0 +1,20 @@
+//
+//  Api_try_Spotify.swift
+//  MUSIC APP
+//
+//  Created by Kaival Patel on 26/02/2023.
+//
+
+import SwiftUI
+
+struct Api_try_Spotify: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Api_try_Spotify_Previews: PreviewProvider {
+    static var previews: some View {
+        Api_try_Spotify()
+    }
+}
